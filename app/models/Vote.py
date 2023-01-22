8dfad0cb341de8a942499d41913b00d9b8bcc550
cykj40 <102045473+cykj40@users.noreply.github.com>
2023-01-22 13:18:56 -0500

    gqa

--- app/models/Vote.py
+++ app/models/Vote.py
@@ -1,8 +1,12 @@
 from app.db import Base
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey 
+from sqlalchemy.orm import relationship
 
 class Vote(Base):
-  __tablename__ = 'votes'
-  id = Column(Integer, primary_key=True)
-  user_id = Column(Integer, ForeignKey('users.id'))
-  post_id = Column(Integer, ForeignKey('posts.id'))
+    __tablename__ = 'votes'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    post = relationship("Post", backref="votes")
+
+  
